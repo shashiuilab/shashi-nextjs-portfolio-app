101e8340085db13699530a8e336efcb412b91534
Shashidhar Reddy <shashidharreddy@192.168.1.102>
2021-02-28 01:17:30 +0000

    Corrected absoluted url to a relative path

--- components/navigation.js
+++ components/navigation.js
@@ -8,7 +8,7 @@ const navigation = ({ linkItems }) => {
         <nav className="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
                 <a className="navbar-brand js-scroll-trigger" href="#page-top">
                     <span className="d-block d-lg-none">Shashidhar Reddy</span>
-                    <span className="d-none d-lg-block"><img className="img-fluid img-profile rounded-circle mx-auto mb-2" src="../public/profile_pic.jpg" alt="" /></span>
+                    <span className="d-none d-lg-block"><img className="img-fluid img-profile rounded-circle mx-auto mb-2" src="../profile_pic.jpg" alt="" /></span>
                 </a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" onClick={() => { setToggleDropdownMenu(!toggleDropdownMenu)}}><span className="navbar-toggler-icon"></span></button>
                 <div className={toggleDropdownMenu ? "collapse navbar-collapse show" : "collapse navbar-collapse"}>

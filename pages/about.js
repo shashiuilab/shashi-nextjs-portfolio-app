86de56caa36c6ca7baa3cdbe69337a35675534c6
Shashidhar Reddy <shashidharreddy@192.168.1.102>
2021-03-01 18:58:36 +0000

    Added role text

--- pages/about.js
+++ pages/about.js
@@ -10,24 +10,31 @@ const about = props => {
                 <h2 className="mb-1">
                     Shashidhar Reddy
                 </h2>
-                <div className="subheading mb-2">
-                    +447459381915, +919010153439 <i className="fas fa-mobile-alt"></i>
-                </div>
-                <div className="subheading mb-2">
-                    <a href="mailTo:shashidhar.loksan@gmail.com">shashidhar.loksan@gmail.com</a>
-                </div>
-                <p className="lead mb-5">
-                JavaScript full stack developer focusing mainly with experience in developing Web/Mobile
-applications and enjoy developing APIs. 10+ Years of Experience of working in different industries banking and financial services, Healthcare,
-e-commerce, manufacturing etc.,. Experience in development of web applications with experience in all latest JavaScript frameworks.
-As a self-motivated applications developer, always look forward to learning, practice and use best industry
-practice and techniques to solve problems and Lead the teams towards achieving project goals.
-                </p>
+                <h3 className="mb-1">
+                    Full Stack Developer
+                </h3>
+                <br />
                 <div className="social-icons">
                 <a className="social-icon" href="mailTo:shashidhar.loksan@gmail.com"><i className="fas fa-envelope"></i></a>
                 <a className="social-icon" target="_blank" href="https://www.linkedin.com/in/shashidhar-lokasani-870bb35a/"><i className="fab fa-linkedin-in"></i></a>
                 <a className="social-icon" target="_blank" href="https://github.com/shashiuilab?tab=repositories"><i className="fab fa-github"></i></a>
                 </div>
+                <br />
+                <div className="subheading mb-2">
+                    <a href="mailTo:shashidhar.loksan@gmail.com">shashidhar.loksan@gmail.com</a>
+                </div>
+                <p className="lead mb-5">
+                JavaScript full stack developer focusing mainly with experience in developing Web/Mobile
+applications and enjoys developing APIs. 10+ Years of Experience of working in different industries banking and financial services, Healthcare,
+e-commerce, manufacturing etc.,. Experience in development of web applications with experience in all latest JavaScript frameworks.
+As a self-motivated applications developer, always look forward to learning, practice and use best industry
+practice and techniques to solve problems and Lead the teams towards achieving project goals.
+                </p>
+                {/* <div className="social-icons">
+                <a className="social-icon" href="mailTo:shashidhar.loksan@gmail.com"><i className="fas fa-envelope"></i></a>
+                <a className="social-icon" target="_blank" href="https://www.linkedin.com/in/shashidhar-lokasani-870bb35a/"><i className="fab fa-linkedin-in"></i></a>
+                <a className="social-icon" target="_blank" href="https://github.com/shashiuilab?tab=repositories"><i className="fab fa-github"></i></a>
+                </div> */}
             <Link to={'/experience'} as={'/experience'} href={'/experience'}>
                 <a className="nav-link js-scroll-trigger next-page-link">View Experience</a>
             </Link>
